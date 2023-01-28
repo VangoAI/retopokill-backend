ecd8b49002be34b0fefd339eb87f3023a7cfbfeb
Rajen Dey <rajen.dey@berkeley.edu>
2023-01-27 16:47:57 -0800

    fix pattern decoding functions, implement feasible

--- app.py
+++ app.py
@@ -15,43 +15,24 @@ CORS(app)
 con = sqlite3.connect("pattern.db", check_same_thread=False)
 cur = con.cursor()
 
-# ALL NUMBERS IN HEX
-FAKE_DEMO_PATTERN_ENCODINGS = [
-    '00000000000000010000000000000002################################0000000000000003000000000000000400000000000000040000000000000005################################################00000000000000060000000000000006000000000000000700000000000000020000000000000007################################################000000000000000800000000000000040000000000000008################0000000000000005################################0000000000000007', # 3x3 grid
-    # '0102######0304########0403####', # 2x2x2x2x2 star
-    # '0102####03040405####06040307020708######090A0B050B########0A09##0B0A##08', # 4x4x2x4
-    # '0102####03040506######0708090504090A0B######0C08070A09080A0509080B0A0C######0B', # 3x3 more complex grid
-    # '01####020304##05060607080809######0605070306##0807090408####', # 3x3x3x1
-    # '0102####030404########0505##02####04', # 3x2x3x2 grid
-    # '0102######0304######05040305######04', # 2x2x3x3
-    # '01####020304####030205040305######04', # 1x2x2x3
-]
-
-#PATTERNS = [Pattern(encoding) for encoding in FAKE_DEMO_PATTERN_ENCODINGS]
-
 @app.route('/get_expanded_patterns', methods=['POST'])
 def get_expanded_patterns():
     sides = request.json
     patch = Patch(sides)
-    matching = []
 
-    res = cur.execute(f"SELECT topology FROM Patches WHERE nCorners='{len(sides)}' LIMIT 100")
-    i = 0
+    res = cur.execute(f"SELECT topology, boundaryIDs FROM Patches WHERE nCorners='{len(sides)}' LIMIT 20")
+
+    matching = []
     for row in res:
-        print(i, row)
-        i += 1
-        
-        encoding = row[0]
-        try:
-            pattern = Pattern(encoding)
-            params = pattern.feasible(patch)
-            if params is not None:
-                expanded_pattern = pattern.expand(params)
-                expanded_pattern.fit(patch)
-                matching.append(expanded_pattern.to_json())
-        except Exception as e:
-            print("error:", i)
-    print(matching)
+        topology_encoding, polychords_encoding = row[0], row[1]
+        print("ENCODINGS:", topology_encoding, polychords_encoding)
+        pattern = Pattern.from_encoding(topology_encoding, polychords_encoding)
+        params = pattern.feasible(patch)
+        print("PARAMS:", params)
+        # if params is not None:
+        #     expanded_pattern = pattern.expand(params)
+        #     expanded_pattern.fit(patch)
+        #     matching.append(expanded_pattern.to_json())
     return matching
 
 if __name__ == '__main__':
--- pattern.py
+++ pattern.py
@@ -1,32 +1,31 @@
 from patch import Patch
 from vertex import Vertex
+from expansion_params import ExpansionParams
 from utils import get_rotations, rotate
+from pulp import LpMaximize, LpProblem, lpSum, LpVariable
+from pulp.constants import LpStatusOptimal
+import pulp
 
 class Pattern:
-    def __init__(self, encoding: str):
-        '''
-        encoding: a string of hex digits representing the unique counterclockwise bfs traversal of the expanded pattern. 
-        the encoding uses hex without the 0x prefix. the special characters '##' represents that this edge is a boundary edge.
+    def __init__(self):
+        self.face_graph = []
+        self.vert_graph = []
+        self.faces = []
+        self.verts = []
+        self.side_polychords = []
+        self.side_lengths = []
+        self.side_verts = []
+        self.num_polychords = 0
+        self.num_verts = 0
 
-        from this encoding, we can recover the pattern.
-        self.faces is a list of lists where self.faces[i] is a list of 4 point indices that make up face i.
-        self.verts is a list of Vertex objects.
-
-        >>> p = Pattern('0102######0304########0403####')
-        >>> p.faces
-        [[0, 1, 2, 3], [2, 1, 4, 5], [3, 2, 6, 7], [2, 5, 8, 9], [6, 2, 9, 10]]
-        >>> p.sides
-        >>> [[0, 1, 4], [4, 5, 8], [8, 9, 10], [10, 6, 7], [7, 3, 0]]
-        '''
-
-        def decode():
+    @staticmethod
+    def from_encoding(topology_encoding: str, polychords_encoding: str):
+        def decode_topology(encoding):
             CHARS_PER_HEX = 16
 
-            if encoding == '':
-                return []
             face_graph = [[-1] * 4 for _ in range(len(encoding) // (3 * CHARS_PER_HEX))]
             last_seen = 0
-            for i in range(len(face_graph)): # 6 because 2 chars for a hex number times 3 numbers per face
+            for i in range(len(face_graph)):
                 for j in range(3): # the string only has 3 numbers per face, the first index is implied
                     s = encoding[i * (3 * CHARS_PER_HEX) + j * CHARS_PER_HEX: i * (3 * CHARS_PER_HEX) + j * CHARS_PER_HEX + CHARS_PER_HEX]
                     if s != '#' * CHARS_PER_HEX:
@@ -36,54 +35,64 @@ class Pattern:
                             face_graph[last_seen][0] = i # set the first index of the new face to the index of the face that points to it
             return face_graph
 
-        def get_faces(face_graph: list[list[int]]):
+        def decode_polychords(encoding):
+            CHARS_PER_HEX = 4
+            
+            side_polychords = [[]]
+            num_polychords = 0
+            i = 0
+            while i < len(encoding):
+                assert encoding[i] == 'x' or encoding[i] == '-'
+                if encoding[i] == 'x':
+                    polychord_num = int(encoding[i + 1: i + 1 + CHARS_PER_HEX], 16)
+                    side_polychords[-1].append(polychord_num)
+                    num_polychords = max(num_polychords, polychord_num)
+                    i += 5
+                elif encoding[i] == '-':
+                    side_polychords.append([])
+                    i += 1
+            return side_polychords, num_polychords
+
+        def get_face_vert_adjacency(face_graph: list[list[int]]):
             '''
             recovers the faces from the face graph
             '''
+            def index_backwards(lst: list, val: int):
+                for i in range(len(lst) - 1, -1, -1):
+                    if lst[i] == val:
+                        return i
+                return -1
+
             INDEX_MAP = [(1, 0), (2, 1), (3, 2), (0, 3)]
             
-            vert_count = 4
-            faces = [[0, 1, 2, 3]]
-            for i in range(1, len(face_graph)):
-                face = []
-                neighbor = face_graph[i][0]
-                reuse_i1, reuse_i2 = INDEX_MAP[face_graph[neighbor].index(i)]
-                face.extend([faces[neighbor][reuse_i1], faces[neighbor][reuse_i2]])
-
-                if 0 <= face_graph[i][1] < i:
-                    neighbor = face_graph[i][1]
-                    _, reuse_i2 = INDEX_MAP[face_graph[neighbor].index(i)]
-                    face.append(faces[neighbor][reuse_i2])
-
-                if 0 <= face_graph[i][2] < i:
-                    neighbor = face_graph[i][2]
-                    reuse_i1, reuse_i2 = INDEX_MAP[face_graph[neighbor].index(i)]
-                    if len(face) == 2:
-                        face.extend([reuse_i1, reuse_i2])
-                    else:
-                        face.append(faces[neighbor][reuse_i2])
-                    faces.append(face)
-                    continue
-
-                if 0 <= face_graph[i][3] < i:
-                    if len(face) == 2:
-                        face.append(vert_count)
-                        vert_count += 1
-                    neighbor = face_graph[i][3]
-                    reuse_i1, _ = INDEX_MAP[face_graph[neighbor].index(i)]
-                    face.append(faces[neighbor][reuse_i1])
-                else:
-                    face.append(vert_count)
-                    vert_count += 1
-                    if len(face) == 3:
-                        face.append(vert_count)
+            vert_count = 0
+            faces = []
+            for i in range(len(face_graph)):
+                face = [-1] * 4
+                for j in range(4):
+                    neighbor = face_graph[i][j]
+                    if 0 <= neighbor < i:
+                        for k in range(j): # to account for case when 2 quads share 2 sides
+                            if face_graph[i][k] == neighbor:
+                                reuse_i1, reuse_i2 = INDEX_MAP[index_backwards(face_graph[neighbor], i)]
+                                if face[j] != -1 and face[j] != faces[neighbor][reuse_i1] or face[(j + 1) % 4] != -1 and face[(j + 1) % 4] != faces[neighbor][reuse_i2]:
+                                    # we chose the wrong one the first time, so we need to swap
+                                    face[k], face[(k + 1) % 4] = faces[neighbor][reuse_i1], faces[neighbor][reuse_i2]
+                                    reuse_i1, reuse_i2 = INDEX_MAP[face_graph[neighbor].index(i)]
+                                break
+                        else:
+                            reuse_i1, reuse_i2 = INDEX_MAP[face_graph[neighbor].index(i)]
+                        face[j], face[(j + 1) % 4] = faces[neighbor][reuse_i1], faces[neighbor][reuse_i2]
+                for j in range(4):
+                    if face[j] == -1:
+                        face[j] = vert_count
                         vert_count += 1
                 faces.append(face)
-            return faces, vert_count
+            return faces, vert_count     
 
         def get_vert_graph(faces: list[list[int]], num_verts: int):
             '''
-            recovers the vertex graph from faces
+            recovers the vertex graph, the dual of the face graph
             '''
             vert_graph = [[] for _ in range(num_verts)]
             for face in faces:
@@ -96,82 +105,86 @@ class Pattern:
                         vert_graph[face[i]].append(cw_neighbor)
             return vert_graph
 
-        def get_sides(face_graph: list[list[int]], faces: list[list[int]]):
-            '''
-            recovers the boundary sides of the pattern from the face graph and faces
-            assumes corners vertices must only be part of one face
-            '''
-            if not face_graph:
-                return []
-
-            corners = set()
-            for i in range(len(face_graph)):
-                for j in range(4):
-                    if face_graph[i][j] == -1 and face_graph[i][j - 1] == -1:
-                        corners.add(faces[i][j])
-
+        def get_side_verts(face_graph, faces, side_lengths):
             border_vert_map = {} # vert -> vert
             for i in range(len(face_graph)):
                 for j in range(4):
                     if face_graph[i][j] == -1:
                         border_vert_map[faces[i][j]] = faces[i][(j + 1) % 4]
-
+            side_lengths = [1] + side_lengths
             sides = []
             curr_vert = 0
+            current_side_length = 0
+            side_length_index = 0
             while True:
-                if curr_vert in corners:
+                if current_side_length == side_lengths[side_length_index] - 1: # in terms of edges
                     sides.append([])
+                    side_length_index += 1
+                    current_side_length = 0
                 sides[-1].append((curr_vert, border_vert_map[curr_vert]))
                 curr_vert = border_vert_map[curr_vert]
+                current_side_length += 1
                 if curr_vert == 0:
                     break
             sides = [[edge[0] for edge in side] + [side[-1][1]] for side in sides]
             return sides
-        
-        face_graph = decode()
-        self.faces, self.num_verts = get_faces(face_graph)
-        self.vert_graph = get_vert_graph(self.faces, self.num_verts)
-        self.sides = get_sides(face_graph, self.faces)
-        self.verts = [Vertex() for _ in range(self.num_verts)]
 
-    def feasible(self, patch: Patch):
+
+        pattern = Pattern()
+        pattern.face_graph = decode_topology(topology_encoding)
+        pattern.side_polychords, pattern.num_polychords = decode_polychords(polychords_encoding)
+        pattern.side_lengths = [len(side_polychord) + 1 for side_polychord in pattern.side_polychords] # in terms of num verts
+        pattern.faces, pattern.num_verts = get_face_vert_adjacency(pattern.face_graph)
+        pattern.verts = [Vertex() for _ in range(pattern.num_verts)]
+        pattern.vert_graph = get_vert_graph(pattern.faces, pattern.num_verts)
+        pattern.side_verts = get_side_verts(pattern.face_graph, pattern.faces, pattern.side_lengths)
+        return pattern
+
+    def feasible(self, patch: Patch) -> ExpansionParams:
         '''
         if the pattern is feasible for the given patch, return the parameters.
         otherwise, return None.
         '''
-        # just checks side lengths for now
-        if len(self.sides) != len(patch.sides):
+        def solve_ilp(patch_side_lengths):
+            model = LpProblem(name="check_feasiblility", sense=LpMaximize)
+            polychord_variables = []
+            for polychord_num in range(self.num_polychords + 1):
+                polychord_variables.append(LpVariable(f"polychord_{polychord_num}", lowBound=0, cat='Integer'))
+            
+            model += lpSum(polychord_variables) # objective function
+
+            for i in range(len(patch_side_lengths)):
+                constraint = patch_side_lengths[i] == self.side_lengths[i] + lpSum([polychord_variables[polychord_num] for polychord_num in self.side_polychords[i]])
+                model += (constraint, f"side_{i}_length_constraint")
+            
+            status = model.solve(pulp.get_solver(pulp.listSolvers(onlyAvailable=True)[0], msg=0))
+            if status == LpStatusOptimal:
+                return [int(polychord_var.value()) for polychord_var in polychord_variables]
             return None
 
-        side_lengths = [len(side) for side in self.sides]
+        # make the patch and pattern have the same number of sides
+        assert len(self.side_lengths) == len(patch.sides)
+
         patch_side_lengths = [len(side) for side in patch.sides]
+        patch_side_lengths_rotations = get_rotations(patch_side_lengths)
+        for i, patch_side_lengths_rotation in enumerate(patch_side_lengths_rotations):
+            solution = solve_ilp(patch_side_lengths_rotation)
+            if solution:
+                return ExpansionParams(solution, i)
+        return None
 
-        rotations = get_rotations(side_lengths)
-        for i in range(len(rotations)):
-            if rotations[i] == patch_side_lengths:
-                return {"rotations": i, 'flip': False}
+    def expand(self, expansion_params: ExpansionParams):
+        def split_polychord(pattern, polychord_num: int, num_splits: int):
+            '''
+            split the polychord with the given number into the given number of polychords.
+            '''
+            pass
 
-        side_lengths = side_lengths[::-1] # flip
-        rotations = get_rotations(side_lengths)
-        for i in range(len(rotations)):
-            if rotations[i] == patch_side_lengths:
-                return {"rotations": i, 'flip': True}
-
-        # compare the lengths of the sides for all rotations of the pattern
-        # for i in range(len(self.sides)):
-
-    def expand(self, params: dict[str, float]):
-        '''
-        expands the pattern into an expanded pattern using the given parameters.
-        '''
-        p = Pattern('')
-        p.num_verts = self.num_verts
-        sides = [s[::-1] for s in self.sides[::-1]] if params["flip"] else self.sides
-        p.sides = rotate([side.copy() for side in sides], params["rotations"])
-        p.vert_graph = [lst.copy() for lst in self.vert_graph]
-        p.faces = [face.copy() for face in self.faces]
-        p.verts = [Vertex() for _ in range(p.num_verts)]
-        return p
+        pattern = self.copy()
+        pattern.rotate(-expansion_params.num_rotations)
+        for polychord_num, num_splits in enumerate(expansion_params.polychord_splits):
+            split_polychord(pattern, polychord_num, num_splits)
+        return pattern
 
     def fit(self, patch: Patch):
         '''
@@ -223,17 +236,19 @@ class Pattern:
         num_iterations = 10
         for _ in range(num_iterations):
             self.verts = laplace_smooth_iter()
+    
+    def rotate(self, num_rotations: int):
+        self.side_polychords = rotate(self.side_polychords, num_rotations)
+        self.side_lengths = rotate(self.side_lengths, num_rotations)
+        
+    def copy(self):
+        pattern = Pattern()
+        pattern.face_graph = [face.copy() for face in self.face_graph]
+        pattern.side_polychords = [side_polychord.copy() for side_polychord in self.side_polychords]
+        pattern.side_lengths = self.side_lengths.copy()
+        pattern.num_polychords = self.num_polychords
+        return pattern
 
     def to_json(self):
         verts = [vert.to_json() for vert in self.verts]
         return {'verts': verts, 'faces': self.faces, 'sides': self.sides}
-
-
-# p = Pattern('0102######0304########0403####')
-# p = Pattern('01##########')
-# p = Pattern('0102####03040405######0606070207######080408##05####07')
-# patch = Patch([[(0, 0, 0), (1, 0, 0), (2, 0, 0), (3, 0, 0)], [(3, 0, 0), (3, 1, 0), (3, 2, 0), (3, 3, 0)], [(3, 3, 0), (2, 3, 0), (1, 3, 0), (0, 3, 0)], [(0, 3, 0), (0, 2, 0), (0, 1, 0), (0, 0, 0)]])
-# p.fit(patch)
-# print(p.faces)
-# print(p.verts)
-# print(p.sides)
